8f9349208df184f52d813c4491027314344fcc90
shivam <chotu.kv@gmail.com>
2017-08-26 11:31:56 +0530

    update script done

---
+++ playground/mongo-update.js
@@ -0,0 +1,21 @@
+const {MongoClient,ObjectID} = require("mongodb");
+//var s = new ObjectID();
+//console.log(s);
+MongoClient.connect('mongodb://localhost:27017/TodoApi',(err,db)=>{
+    if(err){
+        return console.log("Could not connect to the database");
+    }
+    console.log("Connected to the database");
+    db.collection("Users").findOneAndUpdate({name : "Shivam"},{
+        $set: {
+            name : "Shivam12"
+        }
+        ,
+        $inc:{
+            age : -1
+        }
+    },{returnOriginal : false}).then((result)=>{
+        console.log(result);
+   });
+    db.close();   
+});
